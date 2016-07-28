0766ce41bc242a4af66d040eced50c45c80e225d
Kevin Qi <kqi@patientslikeme.com>
2016-07-28 11:58:16 -0700

    pg setup checkpoint

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log(`hello ${'node'}`);
+const { graphql } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+
+const pg = require('pg');
+const config = {
+  database: 'forward'
+};
+const pool = new pg.Pool(config);
+pool.query(`
+  select * from spouses
+`, [])
+  .then(result => console.log(result.rows));
+
+const express = require('express');
+const app = express();
+
+const mySchema = require('./schema');
+
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+
+app.use('/graphql', graphqlHTTP({ schema: mySchema, graphiql: true }));
+
+app.listen(3000, () => {
+  console.log('express is running...');
+});
--- schema.js
+++ schema.js
@@ -1,7 +1,76 @@
 const {
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLList,
 } = require('graphql');
 
-const mySchema = new GraphQLSchema({
+let counter = 42;
+let counters = [42, 43];
+let counterObj = {
+  id: 55,
+  value: 42
+};
+let countersObj = [
+  {
+    id: 55,
+    value: 42
+  },
+  {
+    id: 56,
+    value: 43
+  }
+];
 
+const counterObjType = new GraphQLObjectType({
+  name: 'CounterObj',
+  fields: {
+    id: { type: GraphQLID },
+    value: { type: GraphQLInt }
+  }
 });
+
+const queryType = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {  // what fields are supported on the type
+    hello: {
+      type: GraphQLString,
+      resolve: () => "world"
+    },
+    counter: {
+      type: GraphQLInt,
+      resolve: () => counter++
+    },
+    counters: {
+      type: new GraphQLList(GraphQLInt),
+      resolve: () => counters
+    },
+    counterObj: {
+      type: counterObjType,
+      resolve: () => counterObj
+    },
+    countersObj: {
+      type: new GraphQLList(counterObjType),
+      resolve: () => countersObj
+    }
+  }
+});
+
+const mutationType = new GraphQLObjectType({
+  name: 'RootMutation',
+  fields: {
+    incrementCounter: {
+      type: GraphQLInt,
+      resolve: () => ++counter
+    }
+  }
+});
+
+const mySchema = new GraphQLSchema({
+  query: queryType,
+  mutation: mutationType
+});
+
+module.exports = mySchema;
